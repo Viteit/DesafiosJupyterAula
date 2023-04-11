d81dfb6ff61f394e7406feb195448d9a6ae3b92a
Teste <teste@teste.com.br>
2023-04-11 14:36:17 -0300

    [FEAT] creating loop

--- eduardo.py
+++ eduardo.py
@@ -1,1 +1,5 @@
-print ("hello world")
+"""
+Este código tem a função de retornar na tela uma mensagem dizendo olá mundo. Este código tem a função de retornar na tela uma mensagem dizendo olá mundo. Este código tem a função de retornar na tela
+"""
+
+print("hello world")
